dbedbc8a5d101716a45f21400e5c03b5045b05b2
AmiiRaBellili <belliliamiira@gmail.com>
2021-06-06 13:11:51 +0100

    JWT Authentification not complete prblm in vsc

--- app/User.php
+++ app/User.php
@@ -6,10 +6,14 @@ use App\Model\Like;
 use App\Model\Question;
 use App\Model\Reply;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+
+
+class User extends Authenticatable implements JWTSubject
+
 {
     use Notifiable;
 
@@ -44,5 +48,21 @@ class User extends Authenticatable
     public function questions()
     {
         return $this->hasMany(Question::class);
+    }
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
